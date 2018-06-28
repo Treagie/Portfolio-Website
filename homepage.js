959a5f1081c259f985b8f0524b9e02b11c827ad4
Hannah <37479475+Treagie@users.noreply.github.com>
2018-06-28 21:20:16 +0100

    Edits mainly to JS
    Improved JS code.
    "Say hello" (#hello) button not changing colour - cannot figure out why!!

--- homepage.js
+++ homepage.js
@@ -1,46 +1,74 @@
 //Button effect - change colour when mouse moves over it
-var button = document.querySelector("#button");
+var button = document.querySelector("#hello");
 
-button.addEventListener("mouseenter", function(){
-	this.style.color = "#9FD8CB";
-	this.style.background = "#2d3319";
-	this.style.border = "2px solid #d6e5e3";
+button.addEventListener("mouseover", function(){
+	this.classList.add("dark")
 });
 
 button.addEventListener("mouseleave", function(){
-	this.style.color = "#517664";
-	this.style.background = "#d6e5e3";
-	this.style.border = "2px solid #2d3319";
-})
+	this.classList.remove("dark")
+});
+
 
 //Image effect - possibly change/add text?
-var imageOne = document.querySelector(".one");
-var imageTwo = document.querySelector(".two");
-var imageThree = document.querySelector(".three");
+var imageEffect = document.querySelectorAll(".photo-grid img");
 
-imageOne.addEventListener("mouseenter", function(){
-	this.style.opacity = "0.3";
-});
+for(var i = 0; i < imageEffect.length; i++) {
+	imageEffect[i].addEventListener("mouseenter", function(){
+		this.classList.add("enter");
+	});
+	imageEffect[i].addEventListener("mouseleave", function(){
+		this.classList.remove("enter");
+	});
+}
 
-imageOne.addEventListener("mouseleave", function(){
-	this.style.opacity = "1";
-});
+// OLD CODE BELOW THIS POINT - REPLACED BY THE CODE ABOVE :)
 
-imageTwo.addEventListener("mouseenter", function(){
-	this.style.opacity = "0.3";
-	bristol.textContent;
-});
+//var button = document.querySelector("#hello");
 
-imageTwo.addEventListener("mouseleave", function(){
-	this.style.opacity = "1";
-});
+//button.addEventListener("mouseover", function(){
+//	this.style.color = "#9FD8CB";
+//	this.style.background = "#2D3319";
+//	this.style.border = "2px solid #D6E5E3";
+//});
 
-imageThree.addEventListener("mouseenter", function(){
-	this.style.opacity = "0.3";
-	london.textContent;
-});
+//button.addEventListener("mouseleave", function(){
+//	this.style.color = "#517664";
+//	this.style.background = "#d6e5e3";
+//	this.style.border = "2px solid #2d3319";
+//})
 
-imageThree.addEventListener("mouseleave", function(){
-	this.style.opacity = "1";
-});
+
+
+//var imageOne = document.querySelector(".one");
+//var imageTwo = document.querySelector(".two");
+//var imageThree = document.querySelector(".three");
+
+
+
+//imageOne.addEventListener("mouseenter", function(){
+//	this.style.opacity = "0.3";
+//});
+//
+//imageOne.addEventListener("mouseleave", function(){
+//	this.style.opacity = "1";
+//});
+//
+//imageTwo.addEventListener("mouseenter", function(){
+//	this.style.opacity = "0.3";
+//	bristol.textContent;
+//});
+//
+//imageTwo.addEventListener("mouseleave", function(){
+//	this.style.opacity = "1";
+//});
+//
+//imageThree.addEventListener("mouseenter", function(){
+//	this.style.opacity = "0.3";
+//	london.textContent;
+//});
+//
+//imageThree.addEventListener("mouseleave", function(){
+//	this.style.opacity = "1";
+//});
 
